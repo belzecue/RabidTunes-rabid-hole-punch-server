82f9046892e9fff1614d4fe4d78e8514c3ee0a92
Rabid <jai_mi_to@hotmail.es>
2021-10-30 00:57:51 +0200

    Playing with script

--- start_holepunch_server.sh
+++ start_holepunch_server.sh
@@ -27,12 +27,17 @@ if [ -z "${port}" ]; then
     usage
 fi
 
+debug_text=""
+if [ -n "${debug}" ]; then
+  debug_text="with debug enabled"
+fi
+
 server_pid=$(pgrep -f main_holepunch)
 if [ -z "${server_pid}" ]; then
   # Server is not running
   pip install -r requirements.txt
   nohup python3 main_holepunch.py "$port" $debug >/dev/null 2>&1 &
-  printf "Rabid Hole Punch server started on port %s\n" "$port"
+  printf "Rabid Hole Punch server started ${debug_text} on port %s\n" "$port"
 else
   # Server is running
   if [ -n "${force}" ]; then
@@ -40,7 +45,7 @@ else
     echo "Terminated previous instance of Rabid Hole Punch server"
     pip install -r requirements.txt
     nohup python3 main_holepunch.py "$port" $debug >/dev/null 2>&1 &
-    printf "Rabid Hole Punch server started on port %s\n" "$port"
+    printf "Rabid Hole Punch server started ${debug_text} on port %s\n" "$port"
   else
     printf "Rabid Hole Punch server is already running\n"
   fi
